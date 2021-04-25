befdfcbd68a24a30151b7c468da644ddb7ed7790
Erdem Ün <pclokcer@hotmail.com>
2021-04-25 18:25:30 +0300

    initialize

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const app = express()
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
+//const { v4: uuidV4 } = require('uuid')
+
+app.use(express.static('public'))
+
+
+io.on('connection', socket => {
+
+  socket.on('join-room', (roomId, userId) => {
+
+    console.log('join')
+    socket.join(roomId)
+    socket.to(roomId).broadcast.emit('user-connected', userId)
+
+    socket.on('disconnect', () => {
+      socket.to(roomId).broadcast.emit('user-disconnected', userId)
+    })
+
+  })
+})
+
+server.listen(3000, () => {
+  console.log('Port Listening 3000')
+})
